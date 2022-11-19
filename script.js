ca6f8116f186691e59e9467423c73f42f5025238
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-11-20 04:24:54 +0900

    hhh

--- script.js
+++ script.js
@@ -15,7 +15,58 @@ $(function () {
     });
 });
 
-$(document).ready(function(){
-    document.getElementById("test").innerHTML='<object type="text/html" data="README.md"></object>';
+//$(document).ready(function(){
+//    document.getElementById("test").innerHTML='<object type="text/html" data="README.md"></object>';
 
-})
+//})
+function openTextFile() {
+    var input = document.createElement("input");
+    input.type = "file";
+    input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
+    input.onchange = function (event) {
+        processFile(event.target.files[0]);
+    };
+    input.click();
+}
+function processFile(file) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        output.innerText = reader.result;
+    };
+    reader.readAsText(file, /* optional */ "euc-kr");
+}
+
+
+
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET",file,false);
+    rawFile.onreadystatechange = function() {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status ===200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+
+    rawFile.send(null);
+}
+
+
+
+function testfunc(id, file) {
+
+    
+
+    fetch("footer.html").then(function(response) {
+        response.text().then(function(text) {
+            document.querySelector(id).innerHTML = text;
+        })
+    })
+    
+    
+
+    
+
+}
