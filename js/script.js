c88a8b0616a05b18b3c5ec535b08ae01530c67d6
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-11-28 02:32:13 +0900

    plz

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+var img = new Image();
+img.src = "https://user-images.githubusercontent.com/116702952/204148995-4011f5fc-17eb-42dc-b0b8-3a9e7c592417.png";
+
 var x = canvas.width/2;
-var y = canvas.height/2;
+var y = canvas.height-30;
 
 var dx = 2;
-var dy = -0;
+var dy = -3;
 
-var ballRadius = 10;
+var ballRadius = 1;
 
 function drawBall() {
-ctx.beginPath();
-ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-ctx.fillStyle = "#0095DD";
-ctx.fill();
-ctx.closePath();
 
+ctx.drawImage(img,x,y,30,30);
 }
+
 
 function draw() {
 ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -30,5 +30,7 @@ y += dy;
     if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
         dy = -dy;
     }
+
+    
 }
 setInterval(draw, 10);
