fe36b386873bea6ba7bd5fcb9094f563f2df0418
fantasyshrimp <fantasy.shrimp@gmail.com>
2022-11-28 01:50:57 +0900

    plz

--- js/script.js
+++ js/script.js
@@ -1,63 +1,34 @@
-window.onload = $(function () {
-    $(".left_sub_menu").hide();
-    $(".has_sub").click(function () {
-        $(".left_sub_menu").fadeToggle(300);
-    });
-    // 왼쪽메뉴 드롭다운
-    $(".sub_menu ul.small_menu").hide();
-    $(".sub_menu ul.big_menu").click(function () {
-        $("ul", this).slideToggle(300);
-    });
-    // 외부 클릭 시 좌측 사이드 메뉴 숨기기
-    $('.overlay').on('click', function () {
-        $('.left_sub_menu').fadeOut();
-        $('.hide_sidemenu').fadeIn();
-    });
-});
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
-//$(document).ready(function(){
-//    document.getElementById("test").innerHTML='<object type="text/html" data="README.md"></object>';
+var x = canvas.width/2;
+var y = canvas.height/2;
 
-//})
-function openTextFile() {
-    var input = document.createElement("input");
-    input.type = "file";
-    input.accept = "text/plain"; // 확장자가 xxx, yyy 일때, ".xxx, .yyy"
-    input.onchange = function (event) {
-        processFile(event.target.files[0]);
-    };
-    input.click();
-}
-function processFile(file) {
-    var reader = new FileReader();
-    reader.onload = function () {
-        output.innerText = reader.result;
-    };
-    reader.readAsText(file, /* optional */ "euc-kr");
+var dx = 2;
+var dy = -0;
+
+var ballRadius = 10;
+
+function drawBall() {
+ctx.beginPath();
+ctx.arc(x, y, ballRadius, 0, Math.PI*2);
+ctx.fillStyle = "#0095DD";
+ctx.fill();
+ctx.closePath();
+
 }
 
+function draw() {
+ctx.clearRect(0, 0, canvas.width, canvas.height);
+drawBall();
+x += dx;
+y += dy;
 
-
-function readTextFile(file) {
-    const rawFile = new XMLHttpRequest();
-    rawFile.open("GET",file,false);
-    rawFile.onreadystatechange = function() {
-        if(rawFile.readyState === 4) {
-            if(rawFile.status ===200 || rawFile.status == 201) {
-                const allText = rawFile.responseText;
-                callback(allText);
-            }
-        }
+    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+        dx = -dx;
     }
-
-    rawFile.send(null);
+    if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+        dy = -dy;
+    }
 }
-
-
-
-function testfunc(id, file) {
-    document.getElementById(id).innerHTML +="<p>hihi</p>";
-    
-    
-    //alert(readTextFile('footer.html').innerText);
-}
+setInterval(draw, 10);
